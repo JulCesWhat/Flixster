949a7b5a009f824af5d80d75bf5d87d8de4a6c53
Whatley, Julio (jw970q) <jw970q@att.com>
2019-10-15 21:24:34 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Julio Cesar Whatley on 10/15/19.
+//  Copyright © 2019 Capi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
